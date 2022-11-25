09e579882084efd2c50596909596ebbe0ff0dc39
YajuanDeng <clairedeng713@gmail.com>
2022-11-25 22:30:52 +1000

    finished

---
+++ project2/router.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const axios = require("axios");
+const router = express.Router();
+// GET users
+// http://localhost/api/users
+// http://localhost:3000/users
+
+router.get("/users", async (req,res) => {
+  try {
+    const url = "http://localhost:3000/users";
+    const users = await axios.get(url);
+    console.log("users", users.data);
+    res.status(200).json({
+      msg: "get user succeed",
+      data: users.data
+    })
+  } catch (error) {
+    res.status(500).send("Server Error")
+  }
+})
+
+// GET cards by user id 
+// http://localhost/api/cards?userId=user-uuid-1
+//http://localhost:3000/cards?userId=user-uuid-1
+
+
+router.get("/cards",async(req, res) => {
+  const { userId } = req.query;
+  console.log("userId",userId);
+  try {
+    const url = `http://localhost:3000/cards?userId=${userId}`
+    const cards = await axios.get(url)
+    console.log("cards",cards.data);
+    res.status(200).json({
+      msg: "get cards succeed",
+      data:cards.data
+    });
+  } catch (error) {
+    res.status(500).send("Server Error")
+  }
+  });
+
+
+
+// POST a card to specific user
+// http://localhost/api/cards
+
+router.post("/cards", async (req,res) => {
+  console.log("body",req.body);
+  try {
+    const url = "http://localhost:3000/cards";
+    await axios.post(url, req.body, {
+      headers:{
+        "Content-Type": "application/json",
+      },
+    });
+    res.status(200).json({
+      msg:"Card is created"
+    })  
+  } catch (error) {
+    res.status(500).send("Server Error");
+  }
+})
+
+
+// DELETE card by card id
+// http://localhost/api/cards/:card_id
+
+router.delete("/cards/:card_id", async (req, res) => {
+  try {
+    const url = `http://localhost:3000/cards/${req.params.card_id}`
+    const result = await axios.delete(url);
+    console.log("result",result);
+    res.status(200).json({
+      msg:"Card is deleted",
+    });
+  } catch (error) {   
+    res.status(500).send("Server Error");
+  }
+})
+
+module.exports = router;
---
+++ project2/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors")
+const app = express();
+const router = require("./router");
+
+app.use(cors());
+app.use(express.json());
+app.use("/api",router);
+
+app.listen(80,() => {
+  console.log("Server is running on http://127.0.0.1:80");
+})
